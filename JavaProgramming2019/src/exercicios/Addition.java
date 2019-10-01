c93331b48a9da8a4a3eb0b656b560e64247491df
evitarafadiga <lazvsantos@gmail.com>
2019-10-01 18:29:40 -0300

    Exercício Addition adicionado.

---
+++ JavaProgramming2019/src/exercicios/Addition.java
@@ -0,0 +1,24 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num1;
+		int num2;
+		int soma;
+		
+			System.out.print("Insira o primeiro número: ");
+			num1 = input.nextInt();
+			
+			System.out.print("Insira o segundo número: ");
+			num2 = input.nextInt();
+			
+			soma = num1 + num2;
+			
+			System.out.printf("A soma equivale a %d%n",soma);
+	}
+
+}
