fbd177d6a19e27bf6f6ccb5be3853e2f905bad1b
evitarafadiga <lazvsantos@gmail.com>
2019-10-02 22:19:46 -0300

    Exercício AccountTest adicionado.

---
+++ JavaProgramming2019/src/exercicios/AccountTest.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		Account myAccount = new Account();
+		
+		System.out.printf("O nome inicial é: %s%n%n",myAccount.getName());
+		
+		System.out.println("Por favor insira o nome:");
+		String theName = input.nextLine(); //lê uma linha de texto
+		myAccount.setName(theName); // insere theName em myAccount
+		System.out.println();//gera saída de linha em branco
+		
+		System.out.printf("O nome no objeto myAccount é:%n%s%n",myAccount.getName());
+	}
+
+}
