dc6a6cce2694c5e8f405fb58f2f5a0197f2d6831
Evitarafadiga <lazvsantos@gmail.com>
2019-10-14 20:33:01 -0300

    Classe Student modificada.

--- JavaProgramming2019/src/capitulo004/Student.java
+++ JavaProgramming2019/src/capitulo004/Student.java
@@ -2,9 +2,25 @@ package capitulo004;
 
 public class Student {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String name;
+	private double average;
+	
+	public Student(String name, double average) {
+		
+		this.name=name;
+			
+			if (average> 0.0) 
+				if (average <= 100.0) 
+					this.average = average;
+				
+			
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	public String getName() {
+		return name;
 	}
 
 }
