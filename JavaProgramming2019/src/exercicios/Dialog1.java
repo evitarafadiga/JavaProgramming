bd55b6165913dee7f4d14056089d0838a3e621da
Evitarafadiga <lazvsantos@gmail.com>
2019-10-14 20:06:12 -0300

    Dialog1 adicionado.

---
+++ JavaProgramming2019/src/exercicios/Dialog1.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+import javax.swing.JOptionPane;
+
+public class Dialog1 {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Bem-vindo ao Java");
+		
+	}
+
+}
