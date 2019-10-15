5f65a7d25a865fd9079d694fd776da50b492452f
Evitarafadiga <lazvsantos@gmail.com>
2019-10-15 13:44:20 -0300

    Classe Student finalizada.

--- JavaProgramming2019/src/capitulo004/Student.java
+++ JavaProgramming2019/src/capitulo004/Student.java
@@ -22,5 +22,30 @@ public class Student {
 	public String getName() {
 		return name;
 	}
+	public void setAverage(double studentAverage) {
+		if (average >0.0)
+			if (average <=100.0)
+				this.average = average;
+	}
+	
+	public double getAverage() {
+		return average;
+	}
+	public String getLetterGrade() {
+		String letterGrade = "";//inicializado como uma String vazia
+		
+		if (average >= 90.0)
+			letterGrade = "A";
+		else if (average >= 80.0)
+			letterGrade = "B";
+		else if (average >= 70.0)
+			letterGrade = "C";
+		else if (average >= 60.0)
+			letterGrade ="D";
+		else
+			letterGrade = "F";
+		
+		return letterGrade;
+	}
 
 }
