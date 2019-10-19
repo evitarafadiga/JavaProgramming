44d4364df7f68c235749c80c8888f1046829be81
Evitarafadiga <lazvsantos@gmail.com>
2019-10-18 23:41:46 -0300

    Adicionada Classe DrawPanel.

---
+++ JavaProgramming2019/src/capitulo004/DrawPanel.java
@@ -0,0 +1,20 @@
+package capitulo004;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class DrawPanel extends JPanel{
+	
+	public void paintComponent (Graphics g) {
+		super.paintComponent(g);
+		
+		int width = getWidth(); //largura total
+		int height = getHeight();//altura total
+		
+		//desenha uma linha a partir do canto superior esquerdo até o inferior direito
+		g.drawLine(0, 0, width, height);
+		
+		g.drawLine(0, height, width, 0);
+	}
+
+}
