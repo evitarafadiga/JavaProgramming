1823a2a58690e18e88ae89208559722f7e9b44d5
Evitarafadiga <lazvsantos@gmail.com>
2019-11-22 01:03:42 -0200

    Fim da classe ShapeTest.

--- JavaProgramming2019/src/capitulo005/ShapesTest.java
+++ JavaProgramming2019/src/capitulo005/ShapesTest.java
@@ -1,10 +1,31 @@
 package capitulo005;
 
-public class ShapesTest {
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class ShapesTest 
+{
 
+	public static void main(String[] args) 
+	{
+		//obtém a escolha do usuário
+		String input = JOptionPane.showInputDialog(
+				"Insira 1 para retângulos\n"+
+				"Insira 2 para ovais");
+		
+		int choice = Integer.parseInt(input); //converte a entrada em int
+		
+		//cria o painel com a entrada do usuário
+		Shapes panel = new Shapes(choice);
+		
+		JFrame application = new JFrame();//cria um novo JFrame
+		
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.add(panel);
+		application.setSize(300,300);
+		application.setVisible(true);
+		
+		
 	}
 
 }
