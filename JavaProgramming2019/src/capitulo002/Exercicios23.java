1b1caf8f02bbf4232ce4c0f7fc8c52422a25cb18
Evitarafadiga <lazvsantos@gmail.com>
2020-03-13 03:19:21 -0300

    Exercícios feitos.

---
+++ JavaProgramming2019/src/capitulo002/Exercicios23.java
@@ -0,0 +1,30 @@
+package capitulo002;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Exercicios23 {
+	
+	public static void main(String[] args) {
+		
+		int c, thisIsAVariable,q76354, number;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Insira um inteiro: ");
+		int value = input.nextInt();
+		
+		
+		System.out.println("This is a Java program");
+		
+		System.out.printf("%s%n%s%n","This is a Java","program");
+		
+		if (value != 7) {
+			System.out.printf("%nThe variable number is not equal to 7");
+		}
+		
+		
+		
+	}
+
+}
