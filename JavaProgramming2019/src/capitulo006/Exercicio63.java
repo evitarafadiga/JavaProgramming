8e53e93142e0357be5c694b6e7a0ec5926166cae
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2020-06-10 18:38:11 -0300

    Iniciada resolução
    Exercícios 6.3 e 6.6

---
+++ JavaProgramming2019/src/capitulo006/Exercicio63.java
@@ -0,0 +1,28 @@
+package capitulo006;
+/*
+ * 
+ * Escreva um aplicativo que teste se os exemplos de chamadas de método da classe Math mostrada na Figura 6.2 
+ * realmente produzem os resultados indicados.
+	
+   Forneça o cabeçalho de método para cada um dos seguintes métodos.
+   
+	a) O método hypotenuse, que aceita dois argumentos de ponto flutuante de precisão dupla side1 e side2 e
+	 retorna um resultado de ponto flutuante de dupla precisão.
+	
+	b) O método smallest, que recebe três inteiros x, y e z e retorna um inteiro.
+	
+	c) O método instructions, que não aceita nenhum argumento e não retorna um valor. 
+	[Observação: esses métodos são comumente utilizados para exibição de instruções para o usuário.]
+	
+	d) O método intToFloat , que recebe um argumento number do tipo inteiro e retorna um float.
+
+ */
+
+public class Exercicio63 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ JavaProgramming2019/src/capitulo006/Exercicio66.java
@@ -0,0 +1,20 @@
+package capitulo006;
+
+/*
+ * 
+ * Declare o método sphereVolume para calcular e retornar o volume da esfera. Utilize a seguinte instrução para 
+ * calcular o volume: double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3)
+ * 
+   Escreva um aplicativo Java que solicita ao usuário o raio do tipo double de uma esfera, chama sphereVolume
+   para calcular o volume e exibe o resultado.
+
+ */
+
+public class Exercicio66 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
