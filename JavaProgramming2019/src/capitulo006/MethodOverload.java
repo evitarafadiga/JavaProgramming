77a098d7f10b8b5e732c0276207468652ff3fe86
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2020-06-09 21:47:27 -0300

    Method Overload added
    yes!

---
+++ JavaProgramming2019/src/capitulo006/MethodOverload.java
@@ -0,0 +1,29 @@
+package capitulo006;
+
+public class MethodOverload {
+
+	public static void main(String[] args) {
+
+		System.out.printf("O quadrado do inteiro de 7 equivale a: %d%n",square(7));
+		
+		System.out.printf("O quadrado do duplo de 7.5 equivale a: %f%n",square(7.5));
+		
+	}
+	
+	public static int square(int intValue){
+		
+		System.out.printf("Chamado square com argumento : %d%n", intValue);
+		
+		return intValue * intValue;
+		
+	}
+	
+	public static double square(double doubleValue){
+		
+		System.out.printf("Chamado square com argumento : %f%n", doubleValue);
+		
+		return doubleValue * doubleValue;
+		
+	}
+
+}
