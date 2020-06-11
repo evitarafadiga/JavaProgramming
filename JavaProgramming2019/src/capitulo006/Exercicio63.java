991597ce7b6d4a15ebb6d054f6ebe6f42885bfaf
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2020-06-11 16:46:25 -0300

    Exercicios 63 e 67 feitos.
    Iniciada questão 67.

--- JavaProgramming2019/src/capitulo006/Exercicio63.java
+++ JavaProgramming2019/src/capitulo006/Exercicio63.java
@@ -22,7 +22,66 @@ public class Exercicio63 {
 
 	public static void main(String[] args) {
 		
-
+		System.out.println("Exercicio 6.3 \n");
+		
+		System.out.println(Math.abs(25.7));
+		System.out.println(Math.ceil(25.7));
+		System.out.println(Math.cos(0.0));
+		System.out.println(Math.exp(25.7));
+		System.out.println(Math.floor(25.7));
+		System.out.println(Math.log(25.7));
+		System.out.println(Math.max(25.7, 30.2));
+		System.out.println(Math.min(25.7, 30.2));
+		System.out.println(Math.pow(25.7, 30.2));
+		System.out.println(Math.sin(25.7));
+		System.out.println(Math.sqrt(25.7));
+		System.out.println(Math.tan(25.7));
+		
+		System.out.println("Exercicio 6.3 a) \n");
+		
+		Hypotenuse(5, 3);
+		
+		System.out.println("Exercicio 6.3 b) \n");
+		
+		Smallest(2, 57, 8);
+		
+		System.out.println("Exercicio 6.3 c) \n");
+		
+		Instructions();
+		
+		System.out.println("Exercicio 6.3 d) \n");
+		
+		IntToFloat(5);
+	}
+	
+	public static void Hypotenuse(double side1, double side2) {
+		
+		double res = 0;
+		res += Math.pow(side1, 2);
+		res += Math.pow(side2, 2);
+		res = Math.sqrt(res);
+		
+		System.out.printf("A hipotenusa de %.0f + %.0f equivale a %.1f%n",side1,side2,res);
+	}
+	
+	public static void Smallest(int a, int b, int c) {
+		int min = 0;
+		min = Math.min(a, b);
+		min = Math.min(b, c);
+		min = Math.min(c, a);
+		
+		System.out.printf("O numero minimo equivale a: %d%n",min);
+	}
+	
+	public static void Instructions() {
+		System.out.println("Instrucao recebida.");
+	}
+	
+	public static float IntToFloat(float inteiro) {
+		
+		System.out.print(inteiro);
+		return inteiro;
+		
 	}
 
 }
--- JavaProgramming2019/src/capitulo006/Exercicio66.java
+++ JavaProgramming2019/src/capitulo006/Exercicio66.java
@@ -1,5 +1,9 @@
 package capitulo006;
 
+import java.util.Scanner;
+
+import com.sun.corba.se.spi.orbutil.fsm.Input;
+
 /*
  * 
  * Declare o método sphereVolume para calcular e retornar o volume da esfera. Utilize a seguinte instrução para 
@@ -13,8 +17,21 @@ package capitulo006;
 public class Exercicio66 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Insira o raio da esfera: ");
+		double radius = input.nextDouble();
+		
+		System.out.printf("O volume equivale a: %f%n",SphereVolume(radius));
 
+	}
+	
+	public static double SphereVolume(double radius) {
+
+		double volume = (4.0/3.0) * Math.PI * Math.pow(radius, 3);
+		
+		return volume;
 	}
 
 }
