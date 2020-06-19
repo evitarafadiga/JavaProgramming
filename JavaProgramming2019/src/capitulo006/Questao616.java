0b8b6b5ba82932a3eddbc42884d2d0efe4dd06c6
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2020-06-19 20:06:55 -0300

    Questões resolvidas.
    6.9 a 6.15.

---
+++ JavaProgramming2019/src/capitulo006/Questao616.java
@@ -0,0 +1,18 @@
+package capitulo006;
+
+/*
+ *  Escreva um método isMultiple que determina um par de inteiros se o segundo inteiro for um múltiplo do primeiro. 
+ *  O método deve aceitar dois argumentos inteiros e retornar true se o segundo for um múltiplo do primeiro e false 
+ *  caso contrário. [Dica: utilize o operador de módulo.] 
+ *  Incorpore esse método a um aplicativo que insere uma série de pares inteiros (um par por vez) e determina se o 
+ *  segundo valor em cada par é um múltiplo do primeiro.
+ *  
+ */
+public class Questao616 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ JavaProgramming2019/src/capitulo006/Questao69.java
@@ -0,0 +1,30 @@
+package capitulo006;
+
+import java.util.Scanner;
+
+/*
+ * (Arredondando números) Math.floor pode ser utilizado para arredondar valores ao número inteiro mais 
+ * próximo — por exemplo, y = Math.floor(x + 0.5);
+ * arredondará o número x para o inteiro mais próximo e atribuirá o resultado a y. Escreva um aplicativo 
+ * que lê valores double e utiliza a instrução anterior para arredondar cada um dos números para o 
+ * inteiro mais próximo. 
+ * 
+ * Para cada número processado, exiba ambos os números, o original e o arredondado.
+ */
+
+public class Questao69 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.printf("Digite o numero a ser arredondado: %n");
+		double num = input.nextDouble();
+		
+		double arr = Math.floor(num + 0.5);
+		
+		System.out.printf("O resultado do arredondamento de %f equivale a: %f%n",num,arr);
+		
+
+	}
+
+}
