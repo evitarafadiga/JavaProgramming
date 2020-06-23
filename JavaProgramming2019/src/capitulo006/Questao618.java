54c75ab8529551295acef4e54bdeaa58407d0aa2
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2020-06-22 21:18:29 -0300

    Questões resolvidas.
    6.18 e 6.19.

---
+++ JavaProgramming2019/src/capitulo006/Questao618.java
@@ -0,0 +1,39 @@
+package capitulo006;
+
+import java.util.Scanner;
+/*
+ * Escreva um método squareOfAsterisks que exibe um quadrado sólido (o mesmo número de linhas e colunas) de asteriscos cujo 
+ * lado é especificado no parâmetro inteiro side. Por exemplo, se side for 4, o método deverá exibir
+ *	****
+ *	****
+ *	****
+ *	****
+ * Incorpore esse método a um aplicativo que lê um valor inteiro para side a partir da entrada fornecida pelo usuário e gera 
+ * saída dos asteriscos com o método squareOfAsterisks.
+ */
+
+public class Questao618 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.printf("Digite a quantidade de asteriscos: %n");
+		int side = input.nextInt();
+		
+		squareOfAsterisks(side);
+	}
+
+	public static void squareOfAsterisks(int side) {
+		String astheriscs = "";
+		
+		for (int i = 0; i < side; i++) {
+			
+			for (int j = 0; j < side; j++) {
+				astheriscs += "*";
+			} 
+			astheriscs+="\n";
+			
+			
+		}
+		System.out.printf("%s%n",astheriscs);
+	}
+}
