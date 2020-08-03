81d5ecb3b54be6acc4eb5d51c50b01150a3a827b
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2020-08-03 17:24:11 -0300

    Exercícios finalizados.
    7.09 e 7.11.

---
+++ JavaProgramming2019/src/capitulo007/Exercicio79.java
@@ -0,0 +1,42 @@
+package capitulo007;
+
+import java.security.SecureRandom;
+
+public class Exercicio79 {
+
+	public static void main(String[] args) {
+		final int LINES = 2;
+		final int COLUMNS = 3;
+		int [][] t = new int [LINES][COLUMNS];
+		SecureRandom rand = new SecureRandom();
+	
+		for (int j = 0; j < LINES; j++) {
+			for (int k = 0; k < COLUMNS; k++) {
+				t [j][k] = rand.nextInt(1000);
+			}
+		}
+		displayLine("Array t", t);
+		displayArray("Array t",t);
+	}
+
+	public static void displayLine (String string, int[][] t) {
+		for (int i = 0; i < t.length; i++) {
+			
+			System.out.printf("%d%n",t[0][0]);
+		}
+	}
+	
+	public static void displayArray(String string, int[][] t) {
+		System.out.printf("%n%s: ", string);
+		for (int i = 0; i < t.length; i++) {
+			if (i % 2 == 0)
+				System.out.println();
+			for (int j = 0; j < t.length; j++) {
+				if (j % 2 == 0)
+					System.out.println();
+				System.out.printf("%-19d ", t[i][j]);
+			}
+		}
+			
+	}
+}
