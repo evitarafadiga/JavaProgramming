a2676e446508af57584508e76dc505eddda9cf64
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2020-08-06 18:49:17 -0300

    Exercícios em progresso...
    7.12 e 7.13.

--- JavaProgramming2019/src/capitulo007/Exercicio712.java
+++ JavaProgramming2019/src/capitulo007/Exercicio712.java
@@ -20,14 +20,21 @@ public class Exercicio712 {
 	}
 
 	public static void exibe(int[] value, int[] check) {
+		int temp = 0;
 		boolean b;
-		
-		for (int i = 0; i < value.length; i++) {
-			System.out.printf("%d%n",value[i]);
+		for (int i = 0; i < value.length-1; i++) {
+			for (int j = 0; j < value.length-1-i; j++) {
+				if (value[j] > value[j+1]) {
+					temp = value [j];
+					value[j] = value[j+1];
+					value[j+1] = temp;
+					b = false;
+					System.out.printf("[ %d ] %n",value[j]);				
+				}
+				
+			}
+			
 		}
 		
-		if (b = Arrays.equals(value, check))
-		System.out.printf("%nintArray %s filledIntArray%n",
-		(b ? "==" : "!="));
 	}
 }
