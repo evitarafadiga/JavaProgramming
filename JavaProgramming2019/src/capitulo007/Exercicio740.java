57d3203241bf738ac549657bc42b5528520dda61
Lázaro Santos <lazvsantos@gmail.com>
2020-10-19 19:27:37 -0300

    Exercício 7.40 finalizado.
    Concluída a enquete proposta.

--- JavaProgramming2019/src/capitulo007/Exercicio740.java
+++ JavaProgramming2019/src/capitulo007/Exercicio740.java
@@ -14,52 +14,93 @@ public class Exercicio740 {
 		topics[3] = "Serviços de streaming";
 		topics[4] = "Pessoas virtuais em campanhas";
 
-//		for (int i = 0; i < topics.length; i++) {
-//			System.out.printf("Insira um grau de importancia de 0 a 10 para o tema: %s%n",topics[i]);
-//			Scanner input = new Scanner(System.in);
-//			int nota = input.nextInt();
-//		}
-		
-		for (int k = 0; k < responses.length; k++) {
-			
-			for (int k2 = 0; k2 < responses.length; k2++) {
-				System.out.printf("[%s]", topics[k]);
-				//System.out.printf("[%s] ", responses[k][k2]);
+		for (int i = 0; i < topics.length; i++) {
+			System.out.printf("Insira um grau de importancia de 0 a 10 para o tema: %s%n", topics[i]);
+			Scanner input = new Scanner(System.in);
+			int nota = input.nextInt();
 
-				if (k % 5 == 0)
-					System.out.println();
-			}
-		}
-
-	}
-	
-	public static void outbarChart(int [] array) {
-		int[] frequency = new int [10];
-		
-		for (int i = 0; i < array.length; i++) {
-			switch (array[i]) {
+			switch (nota) {
 			case 1:
-				frequency[i] += 1;
+				responses[i][0] = nota;
+				break;
 			case 2:
-				frequency[i] += 2;
+				responses[i][1] = nota;
+				break;
 			case 3:
-				frequency[i] += 3;
+				responses[i][2] = nota;
+				break;
 			case 4:
-				frequency[i] += 4;
+				responses[i][3] = nota;
+				break;
 			case 5:
-				frequency[i] += 5;
+				responses[i][4] = nota;
+				break;
 			case 6:
-				frequency[i] += 6;
+				responses[i][5] = nota;
+				break;
 			case 7:
-				frequency[i] += 7;
+				responses[i][6] = nota;
+				break;
 			case 8:
-				frequency[i] += 8;
+				responses[i][7] = nota;
+				break;
 			case 9:
-				frequency[i] += 9;
+				responses[i][8] = nota;
+				break;
 			case 10:
-				frequency[i] += 10;
+				responses[i][9] = nota;
+				break;
+			default:
+				break;
+			}
+
+		}
+
+		for (int k = 0; k < 5; k++) {
+
+			for (int k2 = 0; k2 < 10; k2++) {
+
+				if (k2 == 0) {
+					System.out.printf("%n[%-30s]", topics[k]);
+				}
+
+				System.out.printf(" [ %-2s ] ", responses[k][k2]);
 			}
 		}
+		outbarChart(responses);
+	}
+
+	public static void outbarChart(int[][] array) {
+		int[] frequency = new int[10];
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				switch (array[i][j]) {
+				case 1:
+					frequency[i] += 1;
+				case 2:
+					frequency[i] += 2;
+				case 3:
+					frequency[i] += 3;
+				case 4:
+					frequency[i] += 4;
+				case 5:
+					frequency[i] += 5;
+				case 6:
+					frequency[i] += 6;
+				case 7:
+					frequency[i] += 7;
+				case 8:
+					frequency[i] += 8;
+				case 9:
+					frequency[i] += 9;
+				case 10:
+					frequency[i] += 10;
+				}
+			}
+		}
+		System.out.println();
+
 	}
 
 }
