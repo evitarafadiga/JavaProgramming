ae0383d47ef3e559202d21a3094ff9a6f54fa0f8
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2020-11-19 13:11:57 -0300

    Exercício 8.08 e 8.09 finalizados.
    Validação de data e pacote static java.Math.

--- JavaProgramming2019/src/capitulo008/Date.java
+++ JavaProgramming2019/src/capitulo008/Date.java
@@ -7,13 +7,14 @@ public class Date {
 	private static final int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 // construtor: confirma o valor adequado para o mês e dia dado o ano
-	public Date(int month, int day, int year) {
-		// verifica se mês está no intervalo
-		if (month <= 0 || month > 12)
-			throw new IllegalArgumentException("month (" + month + ") must be 1-12");
+	public Date(int day, int month, int year) {
+		
 		// verifica se day está no intervalo para month
 		if (day <= 0 || (day > daysPerMonth[month] && !(month == 2 && day == 29)))
 			throw new IllegalArgumentException("day (" + day + ") out-of-range for the specified month and year");
+		// verifica se mês está no intervalo
+				if (month <= 0 || month > 12)
+					throw new IllegalArgumentException("month (" + month + ") must be 1-12");
 		// verifique no ano bissexto se o mês é 2 e o dia é 29
 		if (month == 2 && day == 29 && !(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)))
 			throw new IllegalArgumentException("day (" + day + ") out-of-range for the specified month and year");
@@ -22,13 +23,32 @@ public class Date {
 		this.year = year;
 		System.out.printf("Date object constructor for date %s%n", this);
 	}
-	
-	public void nextDay(int day, int month, int year) {
-		
+
+	public void nextDay() {
+		if (day < daysPerMonth[month] && !(month == 2 && day == 29)) {
+			this.day += 1;
+		} else {
+			this.day = 1;
+			addMonth();
+
+		}
+	}
+
+	public void addMonth() {
+		if (month < 12) {
+			this.month += 1;
+		} else {
+			this.month = 1;
+			addYear();
+		}
+	}
+
+	public void addYear() {
+		this.year += 1;
 	}
 
 	// retorna uma String no formato mês/dia/ano
 	public String toString() {
-		return String.format("%d/%d/%d", month, day, year);
+		return String.format("%2d/%2d/%4d", day, month, year);
 	}
 }
