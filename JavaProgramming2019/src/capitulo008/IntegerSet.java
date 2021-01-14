c58414c6fa2b2332abc24cdba78442dc3d7c75d3
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2021-01-14 14:55:40 -0300

    Exercícios
    8.13 iniciado e 8.14 finalizado.

---
+++ JavaProgramming2019/src/capitulo008/IntegerSet.java
@@ -0,0 +1,24 @@
+package capitulo008;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class IntegerSet {
+	
+	private int integer;
+	private boolean[] a = new boolean [100];
+	
+	public IntegerSet() {
+		this.integer = integer;
+		Arrays.fill(a, false);
+	}
+	
+	public static void union() {
+		
+	}
+	
+	public static void intersection() {
+		
+	}
+
+}
