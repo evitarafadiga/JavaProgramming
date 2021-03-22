effe69503b14c2f7841043536747a1a59705b59e
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2021-03-22 12:27:33 -0300

    Capítulo 21
    Estrutura de Lista adicionada.

---
+++ JavaProgramming2019/src/com/deitel/datastructures/EmptyListException.java
@@ -0,0 +1,13 @@
+package com.deitel.datastructures;
+
+public class EmptyListException extends RuntimeException {
+// construtor
+	public EmptyListException() {
+		this("List"); // chama outro construtor de EmptyListException
+	}
+
+// construtor
+	public EmptyListException(String name) {
+		super(name + " is empty"); // chama construtor de superclasse
+	}
+} // fim da classe EmptyListException
