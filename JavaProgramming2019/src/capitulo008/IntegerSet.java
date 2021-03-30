2a587c2b109333a646446bc7204dfecf05557754
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2021-03-30 17:44:48 -0300

    Exercício 8.13
    Em progresso...

--- JavaProgramming2019/src/capitulo008/IntegerSet.java
+++ JavaProgramming2019/src/capitulo008/IntegerSet.java
@@ -13,11 +13,49 @@ public class IntegerSet {
 		Arrays.fill(a, false);
 	}
 	
-	public static void union() {
+	public static void isTrue(boolean [] a, int integer) {
+		for (int i = 0; i < a.length; i++) {
+			if (i == integer)
+				a[i] = true;
+		}
+	}
+	
+	public static void isFalse(boolean [] b, int integer) {
+		for (int j = 0; j < b.length; j++) {
+			if (j == integer)
+				b[j] = false;
+		}
+	}
+	public static boolean union(int integer, boolean[]... args) {
+		
+		for (boolean[] a : args) {
+			if (a [integer] == true)
+				return true;
+		}
+		return false;
+	}
+	
+	public static boolean intersection(int integer, boolean []... args) {
+		
+		for (boolean[] a : args) {
+			if (a [integer] == true)
+				return false;
+		}
+		return true;
+	}
+	
+	public void insertElement(int k, boolean[]... args) {
+		
+		int t = args.length + 1;
+		args[t] = args[k];
+		
+		for (int i = k-1; i < k; i++) {
+			Arrays.fill(a, true);
+		}
 		
 	}
 	
-	public static void intersection() {
+	public void deleteElement(int m, boolean[]... a) {
 		
 	}
 
